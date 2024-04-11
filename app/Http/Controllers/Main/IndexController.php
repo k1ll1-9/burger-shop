e4186973b413258c527f95a6d5d713395d0fe4fa
k1ll1-9 <k1ll1-9@yandex.ru>
2024-04-11 10:06:18 +0300

    initial commit

---
+++ app/Http/Controllers/Main/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Main;
+
+use App\Http\Controllers\Controller;
+use Illuminate\View\View;
+
+class IndexController extends Controller
+{
+    public function __invoke(): View
+    {
+        return view('main.index');
+    }
+}
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,44 @@
+@extends('adminlte::page')
+
+@section('title')
+    {{ config('adminlte.title') }}
+    @hasSection('subtitle')
+        | @yield('subtitle')
+    @endif
+@endsection
+
+@section('content_header')
+    @hasSection('content_header_title')
+        <h1 class="text-muted">
+            @yield('content_header_title')
+
+            @hasSection('content_header_subtitle')
+                <small class="text-dark">
+                    <i class="fas fa-xs fa-angle-right text-muted"></i>
+                    @yield('content_header_subtitle')
+                </small>
+            @endif
+        </h1>
+    @endif
+@stop
+
+@section('content')
+    @yield('content-body')
+@stop
+
+
+@section('footer')
+    <div class="float-right">
+        Version: {{ config('app.version', '1.0.0') }}
+    </div>
+
+    <strong>
+        <a href="{{ config('app.company_url', '#') }}">
+            {{ config('app.company_name', 'My company') }}
+        </a>
+    </strong>
+@stop
+
+@section('css')
+    @vite(['resources/css/app.scss'])
+@stop
